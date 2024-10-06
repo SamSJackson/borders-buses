9ecf21a7d689373e25edf692132f78f019b7b3f5
SamSJackson <samuel.jackson.scotland2@gmail.com>
2024-10-06 12:55:44 +0100

    Fixed timezone UTC issues and multiple routes

--- scrape_bus.py
+++ scrape_bus.py
@@ -2,12 +2,11 @@
 
 # LOGGING
 import logging
-from zoneinfo import ZoneInfo
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 # SCRAPING IMPORTS
 import xmltodict
-import functools 
+import functools
 import requests
 import time 
 import os
@@ -38,84 +37,97 @@ WAIT_TIME = 15
 
 time_format = "%Y-%m-%d, %H:%M:%S"
 def string_time_now():
-    tz = ZoneInfo(key='Europe/London')
-    return datetime.strftime(datetime.now(tz=tz), time_format)
+    return datetime.strftime(datetime.now(timezone.utc), time_format)
 
 def get_data():
-    url = f"https://data.bus-data.dft.gov.uk/api/v1/datafeed/?lineRef=51&lineRef=X62&lineRef=67&operatorRef=BORD&api_key={bus_key}"
+    base_url = "https://data.bus-data.dft.gov.uk/api/v1/datafeed/?lineRef={}&operatorRef=BORD&api_key={}"
+    urls = [base_url.format(line, bus_key) for line in ["51", "X62", "67"]]
+
+    url = f"https://data.bus-data.dft.gov.uk/api/v1/datafeed/?lineRef=51&lineRef=X62&operatorRef=BORD&api_key={bus_key}"
     response = requests.get(url)
 
-    if response.status_code != 200:
-        logging.info(f'({string_time_now()}) Request Error. Status Code: {response.status_code}.')
+    responses = [requests.get(url) for url in urls]
+    valid_responses = [response for response in responses if response.status_code == 200]
+    invalid_responses = [response for response in responses if response.status_code != 200]
+
+    if len(valid_responses) == 0:
+        for response in invalid_responses:
+            logging.info(f'({string_time_now()}) Request Error. Status Code: {response.status_code}.')
         return [] 
 
-    data_dict = xmltodict.parse(response.text)
-
-    try:
-        bus_keys = ["Siri", "ServiceDelivery", "VehicleMonitoringDelivery", "VehicleActivity"]
-        buses = functools.reduce(dict.get, bus_keys, data_dict)
-    except KeyError as e:
-        print("Information not found: {e}")
+    data_dicts = [xmltodict.parse(response.text) for response in valid_responses]
 
     information = []
+    for data_dict in data_dicts:
+        try:
+            bus_keys = ["Siri", "ServiceDelivery", "VehicleMonitoringDelivery", "VehicleActivity"]
+            buses = functools.reduce(dict.get, bus_keys, data_dict)
+        except KeyError as e:
+            print("Information not found: {e}")
 
-    for bus in buses:
-        dt_format = "%Y-%m-%dT%H:%M:%S+00:00"
-        timestamp = bus.get('RecordedAtTime') 
+        for bus in buses:
+            dt_format = "%Y-%m-%dT%H:%M:%S+00:00"
+            timestamp = bus.get('RecordedAtTime') 
 
-        journey = bus.get('MonitoredVehicleJourney')
-        if not journey or not timestamp: 
-            logging.info(f'({string_time_now()}) No timestamp or journey information.')
-            continue 
-    
-
-        line = journey.get('PublishedLineName')
-        origin = journey.get('OriginName')
-        destination = journey.get('DestinationName')
-        origin_est_departure = journey.get('OriginAimedDepartureTime')
-        destination_est_arrival = journey.get('DestinationAimedArrivalTime')
-        coords = journey.get('VehicleLocation')
-        if coords:
-            latitude = coords.get('Latitude')
-            longitude = coords.get('Longitude')
-        else:
-            logging.info(f'{timestamp}: No coords')
-            latitude = None
-            longitude = None
-
-        bearing = journey.get('Bearing')
-        number_plate = journey.get('VehicleRef')
+            journey = bus.get('MonitoredVehicleJourney')
+            if not journey or not timestamp: 
+                logging.info(f'({string_time_now()}) No timestamp or journey information.')
+                continue 
             
-        direction = journey.get('DirectionRef')
+            # Bus Data must have been updated within the last 30 seconds of the reques
+            timestamp = datetime.strptime(timestamp, dt_format).replace(tzinfo=timezone.utc)
+            time_last30s = datetime.now(timezone.utc) - timedelta(seconds=30)
 
-        journey_id_keys = ["Extensions", "VehicleJourney", "Operational", "TicketMachine", "JourneyCode"]
-        vehicle_id_keys = ["Extensions", "VehicleJourney", "VehicleUniqueId"]
+            if timestamp < time_last30s:
+                logging.info(f'{datetime.strftime(time_last30s, time_format)} Old data, updated at {datetime.strftime(timestamp, time_format)}.')
+                continue
 
-        journey_id = functools.reduce(dict.__getitem__, journey_id_keys, bus)
-        vehicle_id = functools.reduce(dict.__getitem__, vehicle_id_keys, bus)
-        
-        timestamp = datetime.strptime(timestamp, dt_format)
-        if origin_est_departure:
-            origin_est_departure = datetime.strptime(origin_est_departure, dt_format)
-        if destination_est_arrival:
-            destination_est_arrival = datetime.strptime(destination_est_arrival, dt_format)
-       
-        info_dict = {
-            'time': timestamp,
-            'line_ref': line,
-            'vehicle_id': vehicle_id,
-            'number_plate': number_plate,
-            'journey_id': journey_id,
-            'direction': direction,
-            'origin': origin,
-            'destination': destination,
-            'origin_est_departure': origin_est_departure,
-            'destination_est_arrival': destination_est_arrival,
-            'longitude': longitude,
-            'latitude': latitude,
-            'bearing': bearing
-        }
-        information.append(info_dict)
+            line = journey.get('PublishedLineName')
+            origin = journey.get('OriginName')
+            destination = journey.get('DestinationName')
+            origin_est_departure = journey.get('OriginAimedDepartureTime')
+            destination_est_arrival = journey.get('DestinationAimedArrivalTime')
+            coords = journey.get('VehicleLocation')
+            if coords:
+                latitude = coords.get('Latitude')
+                longitude = coords.get('Longitude')
+            else:
+                logging.info(f'{timestamp}: No coords')
+                latitude = None
+                longitude = None
+
+            bearing = journey.get('Bearing')
+            number_plate = journey.get('VehicleRef')
+                
+            direction = journey.get('DirectionRef')
+
+            journey_id_keys = ["Extensions", "VehicleJourney", "Operational", "TicketMachine", "JourneyCode"]
+            vehicle_id_keys = ["Extensions", "VehicleJourney", "VehicleUniqueId"]
+
+            journey_id = functools.reduce(dict.__getitem__, journey_id_keys, bus)
+            vehicle_id = functools.reduce(dict.__getitem__, vehicle_id_keys, bus)
+            
+            if origin_est_departure:
+                origin_est_departure = datetime.strptime(origin_est_departure, dt_format).replace(tzinfo=timezone.utc)
+            if destination_est_arrival:
+                destination_est_arrival = datetime.strptime(destination_est_arrival, dt_format).replace(tzinfo=timezone.utc)
+           
+            info_dict = {
+                'time': timestamp,
+                'line_ref': line,
+                'vehicle_id': vehicle_id,
+                'number_plate': number_plate,
+                'journey_id': journey_id,
+                'direction': direction,
+                'origin': origin,
+                'destination': destination,
+                'origin_est_departure': origin_est_departure,
+                'destination_est_arrival': destination_est_arrival,
+                'longitude': longitude,
+                'latitude': latitude,
+                'bearing': bearing
+            }
+            information.append(info_dict)
 
     return information
 
@@ -178,12 +190,15 @@ if __name__ == '__main__':
     connection_uri = gen_connection(db_config) 
     engine = sa.create_engine(connection_uri, pool_recycle=360)
     with engine.begin() as conn:
-        query = sa.text(f'SELECT TOP 1 time FROM {db_config["table"]} ORDER BY id ASC')
+        query = sa.text(f'SELECT TOP 1 time FROM {db_config["table"]} ORDER BY time DESC')
         latest_time = pd.read_sql_query(query, conn)['time']
         if len(latest_time) == 0:
-            latest_time = datetime.now()
+            latest_time = datetime.now(timezone.utc)
         else:
             latest_time = latest_time[0]
+  
+    latest_time = latest_time.tz_localize(timezone.utc)
+    bus_data = get_data()
     
     while datetime.now() < end_date:
         bus_data = get_data()
@@ -192,6 +207,7 @@ if __name__ == '__main__':
             continue
 
         timestamp = bus_data[0]['time']
+
         if timestamp <= latest_time: 
             time.sleep(15)
             continue 
@@ -199,6 +215,4 @@ if __name__ == '__main__':
         insert_to_sql(bus_data, db_config)
         latest_time = timestamp 
         logging.info(f'({datetime.strftime(timestamp, time_format)}) Insertion.')
-        print(f"({datetime.strftime(timestamp, time_format)}) Insertion.")
         time.sleep(15)
-
